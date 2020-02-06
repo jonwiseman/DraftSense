1e8290e132c04f7b0a6cc322ec826ca3740450a4
wisemanj <wisemanj@etown.edu>
2020-02-05 21:53:34 -0500

    Comment scraping

---
+++ Scripts/scrape_comments.py
@@ -0,0 +1,67 @@
+import argparse
+import configparser
+import logging
+import praw
+import json
+import time
+
+
+def main():
+    logging.basicConfig(format="%(asctime)s - %(message)s", level=logging.INFO)  # logging info
+    parser = argparse.ArgumentParser(description='Scrape comments from Reddit using PRAW, a registered Reddit account, '
+                                                 'and a valid Reddit dev application.  Scraped comments will be stored '
+                                                 'in .json format (ex. Mahomes.json), in the Data/Threads folder.'
+                                                 'Only one player at a time may be specified.')
+    config = configparser.ConfigParser()  # reading configuration file (to get Reddit account and application details)
+
+    parser.add_argument('n', metavar='NAME', type=str, nargs='+', help='Player name')
+    args = parser.parse_args()  # parse arguments
+
+    name = ' '.join(args.n)  # get name of player to scrape
+    config.read(r'..\configuration.conf')  # read and parse configuration file
+
+    reddit = praw.Reddit(client_id=config['Reddit']['client_id'],
+                         client_secret=config['Reddit']['client_secret'],
+                         password=config['Reddit']['password'],
+                         user_agent=config['Reddit']['user_agent'],
+                         username=config['Reddit']['username'])  # create Reddit instance
+
+    nfl = reddit.subreddit('nfl')
+
+    post_id = None
+    for submission in nfl.search(f'author:NFL_Mod title:{name}'):
+        post_id = submission.id
+
+    logging.info(f'Submission found with id: {post_id}')
+
+    if post_id is None:
+        logging.error('Name not found in u/NFL_Mod post history.  Comment data cannot be scraped.')
+        raise PlayerError
+
+    logging.info('Gathering comments...')
+
+    post = reddit.submission(id=post_id)
+    post.comments.replace_more(limit=None)
+
+    comments = []
+    for top_level_comment in post.comments:
+        for second_level_comment in top_level_comment.replies:
+            comments.append({'comment_id': second_level_comment.id,
+                             'post_id': second_level_comment.submission.id,
+                             'comment': second_level_comment.body})
+        comments.append({'comment_id': top_level_comment.id,
+                         'post_id': top_level_comment.submission.id,
+                         'comment': top_level_comment.body})
+
+    logging.info(f'{len(comments)} comments scraped for {name}')
+    with open(fr'..\Data\Threads\{name.split()[-1]}.json', 'w') as f:
+        json.dump(comments, f)  # dump scraped data in .json format
+
+
+class PlayerError(Exception):
+    """Name not found"""
+
+
+if __name__ == '__main__':
+    main()
+
